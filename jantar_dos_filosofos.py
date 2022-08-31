e389c43eb8885af3bbce29a93c7b90f7e5bf6de1
Fernando Freires de Carvalho <89278418+fernando-freires@users.noreply.github.com>
2022-08-31 10:42:43 -0400

    translating the comments

--- jantar_dos_filosofos.py
+++ jantar_dos_filosofos.py
@@ -8,9 +8,9 @@ class DiningPhilosophers:
     def __init__(self, number_of_philosophers=5, meal_size=7):
         """
         meal_size: jobs
-        philosophers: processes
-        chopsticks: resources (like garf and fork, a philosopher only can eat if he has both oh then)
-        meal_size = 7 & number_of_philosophers = 5 -> [7, 7, 7, 7, 7], thus, each philosopher will have 7 jobs to do
+        philosophers: processos
+        chopsticks: resources (como se fosse garfo e faca, o filósofo só pode comer se ele estiver com ambos)
+        meal_size = 7 & number_of_philosophers = 5 -> [7, 7, 7, 7, 7], então, cada filósofo tem 7 "trabalhos" para fazer
         """
 
         self.meals = [meal_size for _ in range(number_of_philosophers)]
@@ -21,80 +21,81 @@ class DiningPhilosophers:
     def philosopher(self, i):
         j = (i + 1) % 5
         """
-            j will be used to represent the last philosopher of the list, for example,
-            if we have five philosophers [0, 1, 2, 3, 4, 5], the last one will not have a second
-            chopstick, so, using (i+1) % 5 the list will be like: [0, 1, 2, 3, 4, 0] (remember that we're in a rounded table,
-            so the last element have to be relationed to the first element)
+            
+            j será usado para representar o último filósofo da lista, por exemplo,
+            se tivermos cinco filósofos [0, 1, 2, 3, 4, 5], o último não terá um segundo
+            talher, então, usando (i+1) % 5 a lista ficará assim: [0, 1, 2, 3, 4, 0] (lembre-se que estamos em uma mesa redonda,
+            então o último elemento tem que ser relacionado ao primeiro elemento)
         """
 
-        # this loop only will finish when each philosopher finish their meal
+        #  esse loop só terminará quando cada filósofo terminar sua refeição
         while self.meals[i] > 0:
-            self.status[i] = "  P  "  # they all will start thinkinkg (pensando)
-            time.sleep(random.random())  # put the philosopher to sleep
+            self.status[i] = "  P  "  # todos eles irão começar pensando
+            time.sleep(random.random())  # aplicar um sleep entre 0 e 1 no filósofo
             self.status[i] = "  _  "
             if self.chopsticks[i].acquire(
                 timeout=1
-            ):  # check if the chopstick is been used for someone
+            ):  # checar se o talher está sendo utilizado
                 self.chopstick_holders[
                     i
-                ] = " /   "  # if chopstick is not been used, set that to the philosopher
+                ] = " /   "  # se o talher não está sendo usuado, atribua-o para o filósofo
                 time.sleep(
                     random.random()
-                )  # after get a chopstick, put the philosopher to sleep
+                )  # depois de atribuir o talher, aplica-se o sleep no filósofo
                 if self.chopsticks[j].acquire(
                     timeout=1
-                ):  # here we will check if the next chopstick is been used
+                ):  # aqui checaremos se o próximo talher está sendo utilizado ou não
                     self.chopstick_holders[
                         i
-                    ] = " / \\ "  # if chopstick is not been used, set that to the philosopher
+                    ] = " / \\ "  # se ele não estiver sendo utilizado, atribua-o para o filósofo
                     self.status[
                         i
-                    ] = "  C  "  # set de status to: C (comendo) for the philosopher
-                    time.sleep(random.random())  # put the philosopher to sleep
+                    ] = "  C  "  # então atribuiremos o status de: C (comendo) para o filósofo
+                    time.sleep(random.random())  # aplicar um sleep entre 0 e 1 no filósofo
                     self.meals[
                         i
-                    ] -= 1  # subtract one meal of the philosopher that are eating
+                    ] -= 1  # subtrair uma meal do filósofo 
                     self.chopsticks[
                         j
-                    ].release()  # after eat, release the second chopstick
+                    ].release()  # depois de comer, liberar o segundo talher
                     self.chopstick_holders[
                         i
-                    ] = " /   "  # set just one chopstick to the philosopher
-                self.chopsticks[i].release()  # release the first chopstick
+                    ] = " /   "  # atribuir novamente apenas um talher para o filósofo
+                self.chopsticks[i].release()  # liberar o primeiro talher
                 self.chopstick_holders[
                     i
-                ] = "     "  # set none chopstick to the philosopher
-                self.status[i] = "  P  "  # the status will be setted as thinking again
+                ] = "     "  # depois de fazer uma refeição, atribuir nenhum talher ao filósofo
+                self.status[i] = "  P  "  # O status voltará a ser pensando
 
 
 def main():
-    n = 5  # set the number of philosophers
-    m = 7  # set the quantity of meals to each philosopher
+    n = 5  # definir numero de filósofos
+    m = 7  # definir o tamanho da refeição de cada filósofo
 
-    dining_philosophers = DiningPhilosophers(n, m)  # creating the instance
+    dining_philosophers = DiningPhilosophers(n, m)  # instanciando a classe
     philosophers = [
         Thread(target=dining_philosophers.philosopher, args=(i,)) for i in range(n)
-    ]  # thread initialization
-    for philosopher in philosophers:  # will go through any philosopher of the list
-        philosopher.start()  # start the operation
-    while ( sum(dining_philosophers.meals) > 0):  # will be checking if still have meals to be eaten
-        print("=" * (n * 5))  # print the quantity of meals
+    ]  # inicializando a thread
+    for philosopher in philosophers:  # irá percorrer por todos os filósofos
+        philosopher.start()  # começar a thread
+    while ( sum(dining_philosophers.meals) > 0):  # checar se ainda há refeições para serem consumidas
+        print("=" * (n * 5))  # printar a quantidade de refeições
         print(
             "".join(map(str, dining_philosophers.status)),
             " : ",
             str(dining_philosophers.status.count("  C  ")),
-        )  # print the status that the philosopher is
+        )  # printar o status em que o filósofo se encontra
         print("".join(map(str, dining_philosophers.chopstick_holders)))
         print(
             "".join("{:3d}  ".format(m) for m in dining_philosophers.meals),
             " : ",
             str(sum(dining_philosophers.meals)),
-        )  # print the quantity of meals of each philosopher
-        time.sleep(0.1)  # sleep a little after go to the next step
+        )  # printar a quantidade de refeições de cada filósofo
+        time.sleep(0.1)  # um sleep antes de ir para o próximo step
 
     for philosopher in philosophers:
-        philosopher.join()  # ensure there is no parallel processing going on
+        philosopher.join()  # verifica se não há pararelismo durante a execução da thread
 
 
 if __name__ == "__main__":
-    main()  # execute the function
+    main()  # executa a função
